8888e0f6844a682d62de34dbb088dd4de5ac2388
Deepu Roy <d.roy@devon.nl>
2023-12-12 13:27:08 +0530

    new changes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,47 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
+import { faker } from "@faker-js/faker";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Add employee test", async ({ page }) => {
+  // add some exemples of using css selectors in playwright test
+  await test.step("Navigate to login", async () => {
+    await page.goto(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    await expect(page.locator('button[type="submit"]')).toBeVisible();
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await test.step("Login", async () => {
+    await page.getByPlaceholder("Username").fill("Admin");
+    await page.getByPlaceholder("Password").fill("admin123");
+    await page.locator('button[type="submit"]').click();
+    await expect(page.locator("h6").first()).toHaveText("Dashboard");
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await test.step("Navigate to PIM", async () => {
+    await page.locator("a", { hasText: "PIM" }).first().click();
+    await expect(page.locator("h6").first()).toHaveText("PIM");
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const employee = {
+    firstName: faker.person.firstName(),
+    middleName: faker.person.middleName(),
+    lastName: faker.person.lastName(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  };
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await test.step("Add employee", async () => {
+    await page.locator("button", { hasText: "Add" }).first().click();
+    await page.getByPlaceholder("First Name").fill(employee.firstName);
+    await page.getByPlaceholder("Middle Name").fill(employee.middleName);
+    await page.getByPlaceholder("Last Name").fill(employee.lastName);
+    await page.locator("div.user-form-header input").first().check({force: true});
+    await page.locator("div.oxd-input-group", { has: page.locator("label", { hasText: "Username" }),})
+      .fill(employee.username);
+    await page.locator("div.oxd-input-group", { has: page.locator("label", { hasText: "Password" }), })
+      .fill(employee.password);
+    await page.locator("div.oxd-input-group", { has: page.locator("label", { hasText: "Confirm Password" }), })
+      .fill(employee.password);
+  });
 });
